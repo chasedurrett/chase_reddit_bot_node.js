81e6d003ee5131015423a83ca112f9be6bfe1272
chasedurrett <chase.durrett80@gmail.com>
2020-07-08 20:59:41 -0500

    Fixed security issue

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
-const { CLIENT_ID, CLIENT_SECRET, REDDIT_USER, REDDIT_PASS } = process.env;
+const { CLIENT_ID, CLIENT_SECRET, REDDIT_USER, REDDIT_PASS, USER_AGENT } = process.env;
 const { CommentStream } = require("snoostorm");
 const Snoowrap = require("snoowrap");
 const Snoostorm = require("snoostorm");
 
 const r = new Snoowrap({
   userAgent:
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0",
+    USER_AGENT,
   clientId: CLIENT_ID,
   clientSecret: CLIENT_SECRET,
   username: REDDIT_USER,
