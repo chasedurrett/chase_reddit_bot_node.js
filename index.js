72e85b4f3ece85eab59e2c478922a3bb6227a046
chasedurrett <chase.durrett80@gmail.com>
2020-07-08 21:00:24 -0500

    formatting

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
-const { CLIENT_ID, CLIENT_SECRET, REDDIT_USER, REDDIT_PASS, USER_AGENT } = process.env;
+const {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDDIT_USER,
+  REDDIT_PASS,
+  USER_AGENT,
+} = process.env;
 const { CommentStream } = require("snoostorm");
 const Snoowrap = require("snoowrap");
 const Snoostorm = require("snoostorm");
 
 const r = new Snoowrap({
-  userAgent:
-    USER_AGENT,
+  userAgent: USER_AGENT,
   clientId: CLIENT_ID,
   clientSecret: CLIENT_SECRET,
   username: REDDIT_USER,
